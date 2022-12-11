6b272e1a3b5811007f2ec7594d9e5d5587b1bf8f
KajetanPawlowski <101258758+KajetanPawlowski@users.noreply.github.com>
2022-12-11 18:23:34 +0100

    FIX

--- src/common/transferObjects/Conversation.java
+++ src/common/transferObjects/Conversation.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Conversation implements Serializable {
     private int id;
-    private User[] users = new User[2];
+    private ArrayList<User> users = new ArrayList<>();
     private int jobId;
 
     private ArrayList<String> messages = new ArrayList<String>();
@@ -33,13 +33,22 @@ public class Conversation implements Serializable {
         return id;
     }
 
-    public User[] getUsers() {
+    public ArrayList<User> getUsers() {
         return users;
     }
 
     public int getJobId() {
         return jobId;
     }
+    public void setJobId(int newId){
+        jobId = newId;
+    }
 
+    public void setMessages(ArrayList<String> messages) {
+        this.messages = messages;
+    }
 
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
 }
